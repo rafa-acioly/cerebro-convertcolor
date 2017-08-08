c2cf90efb2b3e6da52b8f91aeddbd1fe0552b99f
Rafael <aciolyr@gmail.com>
2017-08-08 10:09:29 -0300

    support for 3-chars HEX

--- src/modules/match.js
+++ src/modules/match.js
@@ -4,7 +4,7 @@ export default {
         const testHSL = /^hsl\(([0-9]{1,3}\%?),\s?([0-9]{1,3}\%?),\s?([0-9]{1,3}\%?)\)/i
         const testHSV = /^hsv\(([0-9]{1,3}\%?),\s?([0-9]{1,3}\%?),\s?([0-9]{1,3}\%?)\)/i
         const testCMYK = /^cmyk\(([0-9]{1,3}),\s?([0-9]{1,3}),\s?([0-9]{1,3}),\s?([0-9]{1,3})\)/i
-        const testHEX = /^#([a-fA-F0-9]{6})/
+        const testHEX = /^#([a-fA-F0-9]{3,6})/
 
         return testRGB.test(term)   ||
                 testHSL.test(term)  ||
